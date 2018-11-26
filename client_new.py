15f641c5fd6670e13f3beb48e57dbdabcd359132
Will Paltz <willpaltz@Wills-MacBook-Pro.local>
2018-11-26 11:35:36 -0600

    functionality to allow for multiple clients to send and receive

--- client_new.py
+++ client_new.py
@@ -2,6 +2,10 @@
 import socket 
 import time
 
+# import thread module 
+from _thread import *
+import threading 
+
 def Main(): 
     # local host IP '127.0.0.1' 
     host = '127.0.0.1'
@@ -14,22 +18,16 @@ def Main():
     # connect to server on local computer 
     s.connect((host,port)) 
   
-
-    while True: 
-  
+    start_new_thread(receive_data, (s,))
+    
+    while True:
         # message you send to server 
         message = input('Enter Emergency Below: \n')
         
         # message sent to server
         s.send(message.encode('ascii')) 
-  
-        # messaga received from server 
-        data = s.recv(1024)
-  
-        # print the received message 
-        # here it would be a reverse of sent message 
-        print('Received from the server :',str(data.decode('ascii'))) 
-  
+        
+        time.sleep(2)
         # ask the client whether he wants to continue 
         ans = input('\nIs the emergency still happening? (y/n) :') 
         if ans == 'y': 
@@ -39,5 +37,19 @@ def Main():
     # close the connection 
     s.close() 
 
+
+def receive_data(s):
+    while True:
+        # messaga received from server
+        data = s.recv(1024)
+        if not data:
+            break
+            
+        # print the received message 
+        # here it would be a reverse of sent message 
+        print('\nALERT: Received from the server :',str(data.decode('ascii')))
+        
+    s.close()
+  
 if __name__ == '__main__': 
     Main()
--- server_new.py
+++ server_new.py
@@ -4,32 +4,36 @@ import socket
 # import thread module 
 from _thread import *
 import threading 
-  
-print_lock = threading.Lock() 
-  
+
+clients = [] # Maintain a list of clients
+#print_lock = threading.Lock() 
+
+
 # thread fuction 
-def threaded(c, my_pid): 
+def threaded(c, my_pid):
     while True: 
-  
         # data received from client 
         data = str(c.recv(1024).decode())
         if not data: 
             print("pid: [" + str(my_pid) + "] has disconnected") 
-              
+            for client in clients:
+                if(client == c):
+                    clients.remove(client)
+                print("Removed client with pid: " + str(my_pid))
             # lock released on exit 
 #            print_lock.release() 
             break
-  
-        # send back reversed string to client 
-        c.send(("Received: " + str(data)).encode('utf-8'))
+        else:
+            for client in clients:
+                client.send(("Received: " + str(data)).encode('utf-8'))
   
     # connection closed 
     c.close() 
-  
-  
+        
 def Main(): 
     host = "" 
-  
+    global clients
+    
     # reverse a port on your computer 
     # in our case it is 12345 but it 
     # can be anything 
@@ -47,14 +51,16 @@ def Main():
   
         # establish connection with client 
         c, addr = s.accept() 
-  
+        clients.append(c)
+        print(c.getsockname())
+        print("Total Clients Connected: " + str(len(clients)))
         # lock acquired by client 
-        print_lock.acquire() 
+#        print_lock.acquire()
         print('Connected to :', addr[0], ':', addr[1]) 
   
         # Start a new thread and return its identifier 
         start_new_thread(threaded, (c, addr[1]))
-        print_lock.release()
+#        print_lock.release()
     s.close() 
   
   
